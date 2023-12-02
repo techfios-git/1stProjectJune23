4cf122c384de7a0f1571672ea12a65bc9001cd69
techfios-git <shihabul.kh@gmail.com>
2023-12-02 13:15:08 -0600

    committed

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,19 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Launch chrome
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+
+}
