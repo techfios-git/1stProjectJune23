255b082a003a066f2db9ff3c4ddf645694f8e26d
techfios-git <shihabul.kh@gmail.com>
2023-12-03 09:59:38 -0600

    2nd commit

---
+++ src/launchBrowser/GitTest2.java
@@ -0,0 +1,24 @@
+package launchBrowser;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		printMe();
+		printMe1();
+		printMe2();
+		printMe3();
+	}
+
+	public static void printMe() {
+		System.out.println("Git Test");
+	}
+	public static void printMe1() {
+		System.out.println("Git Test1");
+	}
+	public static void printMe2() {
+		System.out.println("Git Test2");
+	}
+	public static void printMe3() {
+		System.out.println("Git Test3");
+	}
+}
