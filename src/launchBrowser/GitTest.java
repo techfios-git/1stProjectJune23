dbaab2bf980596c2b6bbd0bc9f5d57a8e23b5a99
techfios-git <shihabul.kh@gmail.com>
2023-12-03 09:38:37 -0600

    1st commit

---
+++ src/launchBrowser/GitTest.java
@@ -0,0 +1,12 @@
+package launchBrowser;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		printMe();
+	}
+
+	public static void printMe() {
+		System.out.println("Git Test");
+	}
+}
